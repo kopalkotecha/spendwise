746f7455f5650c5f9c1a71926f23f30ff8cde8be
portfolio_website <2019kopal.kotecha@ves.ac.in>
2024-12-28 10:50:10 +0530

    initial commit

--- components/header.jsx
+++ components/header.jsx
@@ -3,6 +3,7 @@ import {SignedIn, SignedOut, SignInButton, UserButton} from "@clerk/nextjs";
 import Link from "next/link";
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
+import {LayoutDashboard, PenBox} from "lucide-react";
 
 const Header = () => {
     return (
@@ -13,18 +14,35 @@ const Header = () => {
                     <Image src={"/logo.png"} alt="spendwise logo" height={60} width={200} className="h-12 w-auto object-contain" />
                 </Link>
 
-                <div>
-                    <SignedIn>
+                <div className="flex items-center space-x-4">
+                    <SignedIn >
+                        <Link href="/dashboard" className="text-gray-600 hover:text-blue-600 flex items-center gap-2">
+                        <Button variant="outline">
+                            <LayoutDashboard size={18}/>
+                            <span className="hidden md:inline">Dashboard</span>
+                        </Button>
+                        </Link>
+
+                        <Link href="/transaction/create" className="text-gray-600 hover:text-blue-600 flex items-center gap-2">
+                            <Button className="flex items-center gap-2">
+                                <PenBox size={18}/>
+                                <span className="hidden md:inline">Add Transaction</span>
+                            </Button>
+                        </Link>
 
                     </SignedIn>
 
-            <SignedOut>
+                    <SignedOut>
                 <SignInButton forceRedirectUrl="/dashboard">
                     <Button variant="outline">Login</Button>
                 </SignInButton>
             </SignedOut>
             <SignedIn>
-                <UserButton />
+                <UserButton   appearance={{
+                    elements: {
+                        avatarBox: "w-10 h-10",
+                    },
+                }}/>
             </SignedIn>
                 </div>
             </nav>
