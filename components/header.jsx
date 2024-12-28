2aae23999af3f0b263144490b40dfb83cd269987
portfolio_website <2019kopal.kotecha@ves.ac.in>
2024-12-28 13:00:03 +0530

    initial commit

--- components/header.jsx
+++ components/header.jsx
@@ -4,8 +4,12 @@ import Link from "next/link";
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
 import {LayoutDashboard, PenBox} from "lucide-react";
+import {checkUser} from "@/lib/checkUser";
 
-const Header = () => {
+const Header = async () => {
+
+    await checkUser();
+
     return (
         <div  className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
 
