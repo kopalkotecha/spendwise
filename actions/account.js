79df3000aa3b5366e4c662a428bdd7737ae2096e
portfolio_website <2019kopal.kotecha@ves.ac.in>
2024-12-29 11:18:14 +0530

    initial commit

---
+++ actions/account.js
@@ -0,0 +1,49 @@
+"use server"
+
+import {auth} from "@clerk/nextjs/server";
+import {db} from "@/lib/prisma";
+import {revalidatePath} from "next/cache";
+
+const serializeTransaction = (obj) => {
+    const serialized = { ...obj };
+    if (obj.balance) {
+        serialized.balance = obj.balance.toNumber();
+    }
+    if (obj.amount) {
+        serialized.amount = obj.amount.toNumber();
+    }
+    return serialized;
+};
+
+
+export async function updateDefaultAccount(accountId) {
+    try {
+        const { userId } = await auth();
+        if (!userId) throw new Error("Unauthorized");
+
+        const user = await db.user.findUnique({
+            where: { clerkUserId: userId },
+        });
+
+        if (!user) {
+            throw new Error("User not found");
+        }
+
+        // First unsetting any existing default account
+        await db.account.updateMany({
+            where: { userId: user.id, isDefault: true},
+            data: { isDefault: false },
+        });
+
+        // Then setting the new default account
+        const account = await db.account.update({
+            where: { id: accountId, userId: user.id},
+            data: { isDefault: true },
+        });
+
+        revalidatePath("/dashboard");
+        return { success: true, data: serializeTransaction(account) };
+    } catch (error) {
+        return { success: false, error: error.message };
+    }
+}
---
+++ app/(main)/account/[id]/page.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const AccountsPage = () => {
+    return (
+        <div>AccountsPage</div>
+    )
+}
+export default AccountsPage;
