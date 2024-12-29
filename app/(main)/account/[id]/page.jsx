60ab8b9e71cdfd66dc49720f63769e7004d98f9a
portfolio_website <2019kopal.kotecha@ves.ac.in>
2024-12-29 20:04:37 +0530

    initial commit

--- app/(main)/account/[id]/page.jsx
+++ app/(main)/account/[id]/page.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, {Suspense} from 'react'
+import {getAccountWithTransactions} from "@/actions/account";
+import {notFound} from "next/navigation";
+import TransactionTable from "@/app/(main)/account/_components/transaction-table";
+import {BarLoader} from "react-spinners";
 
-const AccountsPage = () => {
+
+const AccountsPage = async ({params}) => {
+
+    const accountData = await getAccountWithTransactions(params.id);
+
+    if(!accountData){
+        notFound();
+    }
+
+    const {transactions, ...account} = accountData;
+
     return (
-        <div>AccountsPage</div>
+        <div className="space-y-8 px-5 ">
+        <div className="flex gap-4 items-end justify-between">
+            <div>
+                <h1 className="text-5xl sm:text-6xl font-bold gradient-title capitalize">{account.name}</h1>
+                <p className="text-muted-foreground">
+                    {account.type.charAt(0) + account.type.slice(1).toLowerCase()}{" "}
+                    Account
+                </p>
+            </div>
+
+            <div className="text-right pb-2">
+                <div className="text-xl sm:text-2xl font-bold">₹{parseFloat(account.balance).toFixed(2)}</div>
+                <p className="text-sm text-muted-foreground">{account._count.transactions} Transactions</p>
+            </div>
+        </div>
+
+        {/*    Chart Section */}
+
+            <Suspense
+                fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
+            >
+                <TransactionTable transactions={transactions} />
+            </Suspense>
+
+
+        </div>
     )
 }
 export default AccountsPage;
